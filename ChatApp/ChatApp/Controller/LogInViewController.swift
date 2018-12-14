27e5ffd97040973fd8f7300369a2c0e0bd454c77
HellocsWorldgit config --global user.email antonio.94124@gmail.comssh-keygen -t rsa -C antonio.94124@gmail.com <rserran2@mail.sfsu.edu>
2018-12-13 23:17:40 -0800

    changes to login added validation

--- ChatApp/ChatApp/Controller/LogInViewController.swift
+++ ChatApp/ChatApp/Controller/LogInViewController.swift
@@ -14,13 +14,18 @@ import SVProgressHUD
 
 class LogInViewController: UIViewController {
     
+    var validEmail: Bool = false
+    
     //Textfields pre-linked with IBOutlets
     @IBOutlet var emailfield: UITextField!
     @IBOutlet var passwordfield: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginButton.isEnabled = false
        
     }
     
@@ -48,7 +53,40 @@ class LogInViewController: UIViewController {
         
     }
     
+    //CHECKEMAIL: ~check if email is valid
+    @IBAction func doneEditingEmail(_ sender: Any) {
+        if !emailfield.text!.isEmpty {
+            validEmail = isValidEmail(testStr: emailfield.text!)
+            print(validEmail)
+        } else{
+            let alert = UIAlertController(title: "Empty Email", message: "Your Email cannot be empty.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+        
+        if validEmail != true {
+            let alert = UIAlertController(title: "Invalid email", message: nil, preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+        
+    }
     
+    //Mark: ~check password
+    @IBAction func passwordChanged(_ sender: Any) {
+        if passwordfield.text!.count > 5 && validEmail == true {
+            loginButton.isEnabled = true
+        }
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
     
     
 }
